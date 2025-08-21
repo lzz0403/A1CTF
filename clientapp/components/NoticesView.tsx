71cdf315a517913c6ed366d72e324ebc2e5e4e2c
Awoodwhale <woodwhale@qq.com>
2025-08-21 21:56:34 +0800

    🚧 fix: 补充前端ChallengesView/NoticesView下的i18n

--- clientapp/components/NoticesView.tsx
+++ clientapp/components/NoticesView.tsx
@@ -145,6 +145,7 @@ export function NoticesView({ opened, setOpened, notices }: { opened: boolean, s
     messages = notices
 
     const { theme } = useTheme();
+    const { t } = useTranslation("notices_view")
 
     // 消息卡片的可见列表
     const [visible, setVisible] = useState<boolean>(false)
@@ -292,7 +293,7 @@ export function NoticesView({ opened, setOpened, notices }: { opened: boolean, s
                             <div className="w-full flex flex-col items-center">
                                 <div className="container flex flex-col gap-4 items-center justify-center pb-10">
                                     <div className="w-full p-10 pb-0 mb-8 flex items-center">
-                                        <span className="font-bold text-3xl">Announcements</span>
+                                        <span className="font-bold text-3xl">{t("announcement")}</span>
                                         <div className="flex-1" />
                                         <Button className='w-[50px] h-[50px] [&_svg]:size-8 rounded-lg' variant="default"
                                             onClick={() => {
