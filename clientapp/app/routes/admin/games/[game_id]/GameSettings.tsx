9348fbdd22192128ddac5086563a17ae4feb4a9c
Awoodwhale <woodwhale@qq.com>
2025-08-23 11:12:11 +0800

    🚧 fix: 优化 GameSettings 的加载逻辑

--- clientapp/app/routes/admin/games/[game_id]/GameSettings.tsx
+++ clientapp/app/routes/admin/games/[game_id]/GameSettings.tsx
@@ -1,32 +1,40 @@
 import { EditGameView } from "components/admin/EditGameView";
 import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { AdminFullGameInfo } from "utils/A1API";
 import { api } from "utils/ApiHelper";
+import { LoadingPage } from "components/LoadingPage";
 
 
 export default function GameSettings() {
 
-    const { t } = useTranslation()
     const { game_id } = useParams();
+    const navigate = useNavigate()
 
     if (!game_id) {
-        return <div>Not found</div>;
+        navigate("/404")
+        return
     }
 
     const [gameInfo, setGameInfo] = useState<AdminFullGameInfo>();
+    const [gameInfoFetchError, setGameInfoFetchError] = useState(false);
     const gid = parseInt(game_id);
 
     useEffect(() => {
         // Fetch challenge info
         api.admin.getGameInfo(gid).then((res) => {
             setGameInfo(res.data.data);
+        }).catch((_) => {
+            setGameInfoFetchError(true)
         })
     }, [game_id])
 
     if (!gameInfo) {
-        return <div>{t("loading")}</div>;
+        if (gameInfoFetchError) {
+            navigate("/404")
+            return
+        }
+        return <LoadingPage visible={true} />;
     }
 
     return (
