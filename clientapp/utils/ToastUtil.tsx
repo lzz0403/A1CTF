ff8e1ec56112d37bb6dd16581e1bf326d2c634ae
carbofish <carbofish@hotmail.com>
2025-08-30 15:33:56 +0800

    🚧 fix: 修复公告和Hint弹窗无法关闭的问题

--- clientapp/utils/ToastUtil.tsx
+++ clientapp/utils/ToastUtil.tsx
@@ -10,10 +10,10 @@ import { toast as OldToast } from 'sonner'
 export function toastNewNotice({ title, time, openNotices }: { title: string, time: string, openNotices: Dispatch<SetStateAction<boolean>> }) {
 
     OldToast.custom((t) => (
-        <div className="bg-background border-2 rounded-2xl w-full relative">
+        <div className="bg-background/90 border-1 rounded-2xl w-full relative">
             <div className="flex flex-col justify-center gap-2 p-4 pl-4 pr-4">
                 <div className="flex gap-2 items-center">
-                    <MailCheck size={20} />
+                    <MailCheck size={20} className="flex-none" />
                     <span className="text-sm">{i18n.t("toast.notice.title")}</span>
                 </div>
                 <div className="flex gap-2 items-center">
@@ -36,13 +36,13 @@ export function toastNewNotice({ title, time, openNotices }: { title: string, ti
                             <span>{i18n.t("toast.notice.open")}</span>
                         </div>
                     </Button>
-                    <Button variant="outline" onClick={() => toast.dismiss(t)}><CircleX /> {i18n.t("close")}</Button>
+                    <Button variant="outline" onClick={() => OldToast.dismiss(t)}><CircleX /> {i18n.t("close")}</Button>
                 </div>
             </div>
         </div>
     ), {
         duration: 600000,
-        position: "top-center",
+        position: "top-center"
     })
 
 }
@@ -50,10 +50,10 @@ export function toastNewNotice({ title, time, openNotices }: { title: string, ti
 export function toastNewHint({ challenges, time, openNotices: _openNotices }: { challenges: string[], time: number, openNotices: Dispatch<SetStateAction<boolean>> }) {
 
     OldToast.custom((t) => (
-        <div className="bg-background border-2 rounded-2xl w-full relative">
+        <div className="bg-background/80 border-1 rounded-2xl w-full relative">
             <div className="flex flex-col justify-center gap-2 p-4 pl-4 pr-4">
                 <div className="flex gap-2 items-center">
-                    <Text size={20} />
+                    <Text size={20} className="flex-none" />
                     <span className="text-sm">{i18n.t("toast.hint.title")}</span>
                 </div>
                 <div className="flex gap-2 items-center">
@@ -65,7 +65,7 @@ export function toastNewHint({ challenges, time, openNotices: _openNotices }: { 
                     <span className="text-sm">{i18n.t("toast.hint.content", { name: challenges.join(", ") })}</span>
                 </div>
                 <div className="flex gap-4 mt-2 justify-center">
-                    <Button variant="outline" onClick={() => toast.dismiss(t)}><CircleX /> {i18n.t("close")}</Button>
+                    <Button variant="outline" onClick={() => OldToast.dismiss(t)}><CircleX /> {i18n.t("close")}</Button>
                 </div>
             </div>
         </div>
