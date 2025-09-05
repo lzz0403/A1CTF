44da4b4133a0b97f11bc1d3666f3499eb1b89735
Awoodwhale <woodwhale@qq.com>
2025-09-05 18:39:46 +0800

    🚧 fix: 适配 EditChallengeView 的 i18n & 新增题目使用 EditChallengeView

--- clientapp/app/routes/admin/challenges/create/AdminCreateChallenge.tsx
+++ clientapp/app/routes/admin/challenges/create/AdminCreateChallenge.tsx
@@ -1,12 +1,27 @@
 import SafeComponent from "components/SafeComponent"
-import { CreateChallengeView } from "components/admin/CreateChallengeView";
+import { AdminChallengeConfig, ChallengeCategory, FlagType, JudgeType } from "utils/A1API";
+import { EditChallengeView } from "components/admin/EditChallengeView";
 
-export default function Home () {
-
+export default function Home() {
+    const challengeConfig: AdminChallengeConfig = {
+        name: "",
+        description: "",
+        category: ChallengeCategory.MISC,   // 默认MISC
+        judge_config: {
+            judge_type: JudgeType.DYNAMIC,
+            judge_script: null,
+            flag_template: ""
+        },
+        container_config: [],
+        attachments: [],
+        flag_type: FlagType.FlagTypeStatic, // 默认静态flag
+        allow_wan: false,
+        allow_dns: false,
+    }
     return (
         <div className="p-0 h-screen flex flex-col">
             <SafeComponent animation={false}>
-                <CreateChallengeView  />
+                <EditChallengeView challenge_info={challengeConfig} isCreate={true} />
             </SafeComponent>
         </div>
     );
